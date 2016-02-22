5a113bf7b3ddd37a53ffe8d52fa26380f567689e
Trey Watford <Trey.Watford@acstechnologies.com>
2016-02-22 11:24:27 -0500

    public release 1

---
+++ README.md
@@ -0,0 +1,24 @@
+remoteUpdater
+=============
+
+Author: Trey Watford
+Email: treyjustinwatford@gmail.com
+
+This is a simple file system scanner written in GO that can be used to update git repository remotes.
+
+To use this program please follow the steps listed below:
+1. Run the program from the command line passing in the following values
+  1. -root=your_root_directory
+  2. -replace=text_to_replace
+  3. -new_text=text_to_replace_with
+  4. -force=true or false Note: true will not prompt before modification.
+2. If you chose -force=false, when prompted press y [ENTER] to replace text or n [ENTER] to leave text unchanged.
+3. After the program is complete press any key to exit.
+
+Default values for command line flags:
+  * -root=/ for unix -root=C:\ for windows
+  * -replace=     #empty string by default
+  * -new_text=    #empty string by default
+  * -force=false
+
+This program was created during an internship with ACS Technologies. For more information on careers at ACS Technologies please visit http://www.acstechnologies.com/company/careers.
---
+++ remoteUpdater.go
@@ -0,0 +1,133 @@
+/*
+ *Author: Trey Watford
+ * Email: treyjustinwatford@gmail.com
+ * Description: This is a tool designed to find all .git/config files on a
+ * file system and replace given text with a new string.  This tool is meant to be
+ * used from the command line and will work on both Windows and Unix based systems.
+ * Feel free to use this tool as needed and please let me know of any bugs so that
+ * they can be fixed.
+ *
+ */
+package main
+
+import (
+  "io/ioutil"
+  "strings"
+  "flag"
+  "path/filepath"
+  "fmt"
+  "os"
+  "runtime"
+)
+//define flags with default values
+var root = flag.String("root", getRoot(), "starting directory")
+var replace = flag.String("replace", "", "text to be replaced")
+var new_text = flag.String("new_text", "", "text to replace with")
+var force = flag.Bool("force", false, "true forces file modification without prompt")
+
+//main driver function
+func main() {
+  text := ""  //dummy string to hold output open
+  flag.Parse()  //parse flags
+  if(flag.NFlag()<= 3){
+    err := filepath.Walk(*root, visit)
+    if(err != nil){
+      fmt.Printf("Error, returned %v\n", err)
+    }else{
+      fmt.Print("\nAll remotes updated successfully.\n")
+      fmt.Print("Press any key then [ENTER] to quit.\n")
+      fmt.Scanf("%s", &text)  //used to hold output screen open
+    }
+  }
+}
+
+//function to return the root directory of a file system depending on OS
+func getRoot() string{
+  if runtime.GOOS == "windows" {
+    return "C:\\"
+  }else{
+    return "/"
+  }
+}
+
+func visit(path string, f os.FileInfo, err error) error {
+  fmt.Printf("Visited: %s\n", path)
+  desiredFile := filepath.FromSlash(".git/config") //returns os-specific path
+  if strings.Contains(path, desiredFile){
+    err:= modifyRemotes(path, *replace, *new_text)
+    if(err != nil){
+      panic(err)
+    }
+  }
+  return nil
+}
+
+//modify remote repo changes occurrences of one string to another in a given file
+func modifyRemotes(path string, match string, newStr string) error {
+  //create input file
+  input, err := ioutil.ReadFile(path)
+  if err != nil {
+    return err
+  }
+  //alert user of file read
+  fmt.Printf("\n\nread file %s.", path)
+  lines := strings.Split(string(input), "\n") //split file into lines
+  indices := []int{} //index slice for matched lines
+
+  //count lines containing replace and copy indices into index slice
+  for i, line := range lines {
+    if strings.Contains(line, match) {
+      indices = append(indices, i)
+    }
+  }
+  if len(indices) > 0{
+    //alert user that a number of lines in file will be modified
+    fmt.Printf("\n%v lines will be modified.", len(indices))
+    if *force != true{
+      if getUserInput(){
+        for v := range indices {
+          fmt.Printf("modifying line %s --->", lines[indices[v]])
+          lines[indices[v]] = strings.Replace(lines[indices[v]], match, newStr, 1)
+          fmt.Printf("%s\n", lines[indices[v]])
+        }
+        }else{
+          fmt.Printf("\nNothing to modify, closing file...")
+        }
+        }else{
+          for v := range indices {
+            fmt.Printf("modifying line %s --->", lines[indices[v]])
+            lines[indices[v]] = strings.Replace(lines[indices[v]], match, newStr, 1)
+            fmt.Printf("%s\n", lines[indices[v]])
+          }
+        }
+      }
+
+      //overwrite file
+      output := strings.Join(lines, "\n")
+      err = ioutil.WriteFile(path, []byte(output), 0644)
+      if err != nil {
+        return err
+      }
+      fmt.Printf("\nFile saved --success!\n\n")
+      return nil
+    }
+
+//function returns true if user inputs y and false for n
+func getUserInput() bool {
+  input := ""
+  loop := true
+  for loop == true{
+    fmt.Print("Continue [y/n]? : ")
+    fmt.Scanf("%s", &input)
+    input = strings.ToLower(input)
+    if strings.Compare(input, "y") == 0{
+      loop = false
+    }else if strings.Compare(input, "n") == 0{
+      fmt.Printf("\nNo changes will be made.")
+      return false
+    }else{
+      fmt.Printf("\nPlease enter y or n\n")
+    }
+  }
+  return true
+}
